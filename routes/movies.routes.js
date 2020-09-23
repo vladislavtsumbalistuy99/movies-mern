6e1c6966a118e8fa73d0a6db1e66499f52c58bb6
Vladislav <vlastelin1599@gmail.com>
2020-09-24 02:53:24 +0300

    check the identity of movies

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const Movies = require("../models/Movies");
-const {check, validationResult} = require('express-validator')
+const { check, validationResult } = require("express-validator");
 const router = Router();
 
 router.get("/", async (req, res) => {
@@ -19,22 +19,23 @@ router.get("/", async (req, res) => {
 router.post(
   "/addMovie",
   [
-    check('title').not().isEmpty().withMessage('Title can\'t be empty'),
-    check('year').matches(/(18[5-8][0-9]|189[0-9]|19[0-9]{2}|20[01][0-9]|2020)/).withMessage('Year must be between 1850 and 2020'),
-    check('stars').not().isEmpty().withMessage('Stars can\'t be empty'),
+    check("title").not().isEmpty().withMessage("Title can't be empty"),
+    check("year")
+      .matches(/(18[5-8][0-9]|189[0-9]|19[0-9]{2}|20[01][0-9]|2020)/)
+      .withMessage("Year must be between 1850 and 2020"),
+    check("stars").not().isEmpty().withMessage("Stars can't be empty"),
   ],
   async (req, res) => {
     try {
-      const errors = validationResult(req)
+      const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(400).json({
           errors: errors.array(),
-          message: errors.array()[0].msg
-        })
+          message: errors.array()[0].msg,
+        });
       }
       const { id, title, year, format, stars } = req.body;
       const arrStars = stars.split(" ");
-     
       let result = [];
       for (let str of arrStars) {
         if (!result.includes(str)) {
@@ -42,6 +43,17 @@ router.post(
           result.push(str);
         }
       }
+
+      const checkTitle = await Movies.find({ title: title });
+      if(checkTitle.length >0){
+        for(let i =0;i<checkTitle.length;i++){
+          if(year == checkTitle[i].year || stars.toString() == checkTitle[i].stars.toString()){
+            res.status(200).json({ message: "Oops, this moovie already exist!" });
+            return
+          }
+        } 
+      }
+      // const checkYear = await Movies.find({ title: year });
 
       const movie = new Movies({ id, title, year, format, stars: result });
       await movie.save();
@@ -72,10 +84,10 @@ router.get("/search/:param", async (req, res) => {
     const moviesByTitle = await Movies.find({ $text: { $search: param } });
     const moviesBystar = await Movies.find({ stars: param });
     let movies;
-    if (moviesByTitle.length >0){
-      movies = moviesByTitle
+    if (moviesByTitle.length > 0) {
+      movies = moviesByTitle;
     } else {
-      movies = moviesBystar
+      movies = moviesBystar;
     }
     res.json(movies);
   } catch (e) {
@@ -88,8 +100,9 @@ router.get("/sort", async (req, res) => {
     Movies.find(function (err, movies) {
       movies.sort((a, b) => {
         let x = a.title.toLowerCase();
-        let y = b.title.toLowerCase()
-        return( x > y ? 1 : -1)});
+        let y = b.title.toLowerCase();
+        return x > y ? 1 : -1;
+      });
       res.json(movies);
     });
   } catch (e) {
